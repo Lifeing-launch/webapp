9877a6137b99ceceec200a0fceb3090a2cf634de
Karen Okonkwo <karenokonkwo29@gmail.com>
2025-06-16 01:22:16 +0100

    Fix vercel URL

--- src/utils/urls.tsx
+++ src/utils/urls.tsx
@@ -25,7 +25,7 @@ export function getSiteUrl() {
   }
 
   if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-    return process.env.NEXT_PUBLIC_VERCEL_URL;
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   }
 
   return "http://localhost:3000";
