47a4f6ae335ca64a6097fd3c0ded17b94dd8b3f9
Karen Okonkwo <karenokonkwo29@gmail.com>
2025-08-04 02:56:05 +0100

    Temporarily remove sso login

--- src/components/auth/login/form.tsx
+++ src/components/auth/login/form.tsx
@@ -69,7 +69,8 @@ export function LoginForm({
               <SubmitButton className="w-full" pendingText="Logging in...">
                 Login
               </SubmitButton>
-              <div className="relative text-sm text-center after:absolute after:inset-0  after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+              {/* TODO: Finalize SSO/Social Login */}
+              {/* <div className="relative text-sm text-center after:absolute after:inset-0  after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                 <span className="text-muted-foreground relative z-10 bg-background px-2">
                   Or continue with
                 </span>
@@ -86,8 +87,8 @@ export function LoginForm({
                   </svg>
                   Login with Facebook
                 </Button>
-              </div>
-              <div className="text-center text-sm">
+              </div> */}
+              <div className="text-center text-sm mt-10">
                 Don&apos;t have an account?{" "}
                 <Link href="/signup" className="underline">
                   Sign up
